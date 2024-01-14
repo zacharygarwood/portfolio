63e281fcc9ad32b42e5177f000044d6ed86ba015
Zachary Garwood <garwoodzachary@gmail.com>
2024-01-14 16:00:27 -0600

    Redesigned site

--- src/routes/projects/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Project from '$lib/components/Project.svelte';
-    export let data
-</script>
-    
-{#each data.projects as project}
-    <Project path={project.path} title={project.meta.title} description={project.meta.description} />
-{/each}
