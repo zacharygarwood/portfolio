01a9d0f7b2b9d7077f2e264f4cbe301a9565e842
Zachary Garwood <garwoodzachary@gmail.com>
2024-07-05 16:45:59 -0500

    Majority of redesign

---
+++ src/lib/components/projects.svelte
@@ -0,0 +1,15 @@
+<script>
+    const projects = [
+        { image: "/assets/flounders-best-game.gif", href: "https://github.com/zacharygarwood/Flounder", size: "regular" },
+        { image: "/assets/flounders-best-game.gif", href: "https://github.com/zacharygarwood/Flounder", size: "large" },
+        { image: "/assets/flounders-best-game.gif", href: "https://github.com/zacharygarwood/Flounder", size: "regular" },
+    ]
+</script>   
+
+<div class="grid grid-cols-3 gap-4">
+    {#each projects as project, index}
+        <a href={project.href} target="_blank" rel="noreferrer" class="{project.size === 'large' ? 'col-span-2 row-span-2' : 'col-span-1 row-span-1'} relative">
+            <img src={project.image} alt="Image" class="rounded-lg w-full h-auto hover:outline">
+        </a>
+    {/each}
+</div>
---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+
+export const PERSONAL_LINKS = {
+    github: "https://github.com/zacharygarwood",
+    email: "mailto:garwoodzachary@gmail.com",
+    linkedin: "https://linkedin.com/in/zachary-garwood",
+    resume: "/assets/zachary_garwood_resume.pdf"
+}
+
+export const JOB_WEBSITES = {
+    amazon_freight: "https://freight.amazon.com/",
+    aws_proserve: "https://aws.amazon.com/professional-services/",
+    principal: "https://www.principal.com/",
+    iowa_state: "https://se.iastate.edu/",
+}
+  
+  
